a605a4f91aad58b161691a9307e3c9c55d76ff17
mingu-beep <bramurve@gmail.com>
2024-08-28 21:32:45 +0900

    Security Setting
    의존성 및 Configure 설정 추가

---
+++ src/main/java/min/project/muse/service/UserDetailService.java
@@ -0,0 +1,24 @@
+package min.project.muse.service;
+
+import lombok.RequiredArgsConstructor;
+import min.project.muse.domain.user.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@RequiredArgsConstructor
+@Service
+// 스프링 시큐리티에 사용자 정보를 가져오는 인터페이셔
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    // 사용자 이름으로 사용자의 정보를 가져오는 메서드
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalArgumentException((username)));
+    }
+}
