6426ae876878065672cbb1e68dc0173de473655f
mingu-beep <bramurve@gmail.com>
2025-02-14 22:43:43 +0900

    admin 페이지 접근 권한 추가

--- src/main/java/min/project/muse/config/WebSecurityConfig.java
+++ src/main/java/min/project/muse/config/WebSecurityConfig.java
@@ -73,7 +73,10 @@ public class WebSecurityConfig {
                                 new AntPathRequestMatcher("/musics", "GET"),
                                 new AntPathRequestMatcher("/popular"),
                                 new AntPathRequestMatcher("/all")
-                        ).permitAll() // 누구나 접근이 가능하다.
+                        ).permitAll()
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/admin/**")
+                        ).hasRole("ADMIN")
                         .anyRequest().authenticated() // 위에 설정한 url 이외의 요청에 대해서는 인증이 필요하다.
                 )
                 .formLogin(formLogin -> formLogin // 폼 기반 로그인 설정
@@ -92,7 +95,7 @@ public class WebSecurityConfig {
                         .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint.userService(oAuth2UserCustomService))
                 )
                 .csrf(AbstractHttpConfigurer::disable)// csrf 비활성화
-                .exceptionHandling(e-> e.accessDeniedPage("/"))
+                .exceptionHandling(e-> e.accessDeniedPage("redirect:/"))
                 .build();
 
     }
